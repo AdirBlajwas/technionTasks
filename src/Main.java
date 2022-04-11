8d4d13e7360814975305b03b0fd57ea541204014
AdirBlajwas <adir.blajwas@campus.technion.ac.il>
2022-04-11 15:41:10 +0300

    added most of the javadoc

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
     static final int INVALID = 0;
     static final char VALID_CELL = '▮';
     static final char INVALID_CELL = '▯';
+    static final char SPACE = ' ';
+
+
 
     public static Scanner scanner;  // Note: Do not change this line.
 
@@ -21,15 +24,21 @@ public class Main {
         }
     }
 
+    /**
+     * @param xy an empty array (len=2) which used to save the numbers from the input
+     * @param input the input string that was received from the user
+     * The function reads from a string in a format of - "m X n" or "x, y" the integers (x,y / m,n)
+     */
     public static void getNumFromScanner(int[] xy, String input) {
+
         String sX = "", sY = "" ;
         int i = 0;
-        while(input.charAt(i) != ' ' && input.charAt(i) != ',')
+        while(input.charAt(i) != SPACE && input.charAt(i) != ',')
         {
             sX = sX + input.charAt(i);
             i++;
         }
-        while(input.charAt(i) == ' ' || input.charAt(i) == ',' || input.charAt(i) == 'X') i++;
+        while(input.charAt(i) == SPACE || input.charAt(i) == ',' || input.charAt(i) == 'X') i++;
         while(i < input.length()){
             sY = sY + input.charAt(i);
             i++;
@@ -37,6 +46,8 @@ public class Main {
         xy[0] = Integer.parseInt(sX);
         xy[1] = Integer.parseInt(sY);
     }
+
+
 
     public static void printBoard(int[][] board, int m, int n) {
         for (int i = 0; i < m; i++) {
@@ -47,33 +58,46 @@ public class Main {
                     System.out.print(VALID_CELL);
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 
 
-
-
+    /**
+     *
+     * @param row the row number which is checked if it's out of bounds.
+     * @param col the column number which is checked if it's out of bounds.
+     * @param m the number of rows in the matrix.
+     * @param n the number of columns in the matrix.
+     * @return true if the position is in the bounds of a matrix[m][n] false otherwise.
+     */
     public static boolean inBounds(int row, int col, int m, int n) {
         return ((row >= 0) && (col >= 0) && (col < n) && (row < m));
     }
-
-    public static int updateStudentStatus(int[][] board, int row, int col, int validStudentsNum) {
+    /** The function updates the validation of a student in board[row][col]*/
+    public static void updateStudentStatus(int[][] board, int row, int col) {
         if (board[row][col] == INVALID) {
             board[row][col] = VALID;
-            validStudentsNum++;
         } else {
             board[row][col] = INVALID;
-            validStudentsNum--;
         }
-        return validStudentsNum;
     }
 
-    public static int initStudents(int[][] board, int m, int n) {// check
-        int row, col, validStudentsNum = 0;
+    /**
+     *
+     * @param board the game board
+     * @param m the number of rows in board
+     * @param n the number of columns in board
+     * xy - an integers array[2] used to retract the locations(integers) from the input(string)
+     * The function initialize the board according to the president's input
+     * gets from the user (The president) a series of locations(strings) for the valid students until "Yokra" is entered.
+     * The function updates the board accordingly.
+     */
+    public static void initStudents(int[][] board, int m, int n) {
+        int row, col;
         int [] xy = new int[2];
         System.out.println("Dear president, please enter the cell’s indexes.");
-        String cellsIndexes = scanner.nextLine(); // check
+        String cellsIndexes = scanner.nextLine();
 
         while (!cellsIndexes.equals("Yokra")) {
             getNumFromScanner(xy,cellsIndexes);
@@ -81,7 +105,7 @@ public class Main {
             col = xy[1];
 
             if (inBounds(row, col, m, n)) {
-                validStudentsNum = updateStudentStatus(board, row, col, validStudentsNum);
+                updateStudentStatus(board, row, col);
                 System.out.println("Dear president, please enter the cell’s indexes.");
             } else {
                 System.out.println("The cell is not within the board’s boundaries, enter a new cell.");
@@ -89,8 +113,6 @@ public class Main {
 
             cellsIndexes = scanner.nextLine();
         }
-
-        return validStudentsNum;
     }
 
     public static int semesterChanges(int[][] board, int[][] validFriends, int m, int n) {
@@ -110,6 +132,15 @@ public class Main {
         return numOfChanges;
     }
 
+
+    /**
+     *
+     * @param board The game board
+     * @param validFriends A matrix[m][n] used to save for each student in board the number of valid friends he has.
+     * @param m the number of rows in board
+     * @param n the number of columns in board
+     * @return the number of valid students on the board.
+     */
     public static int countValidFriends(int[][] board, int[][] validFriends, int m, int n) {
 
         int[][] locations = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
@@ -130,21 +161,42 @@ public class Main {
         return validStudentsNum;
     }
 
+    /**
+     * @param board the game board
+     * @param m the number of rows in board
+     * @param n the number of columns in board
+     * @param validStudentsNum the number of valid students on the board.
+     * @param semester the number of the current semester.
+     *  The function prints the current status of the game - the semester number, the board, number of valid students.
+     */
     public static void printStatus(int[][] board,int m, int n, int validStudentsNum, int semester) {
         System.out.println("Semester number " + semester + ":");
         printBoard(board, m, n);
         System.out.println("Number of students: " + validStudentsNum);
-        System.out.println("");
+        System.out.println();
 
     }
 
-    public static boolean isGameContinues(int[][] board , int m, int n, int semesterNum, int validStudentsNum, int changes) {
+    /**
+     * @param board the game board
+     * @param m the number of rows in board
+     * @param n the number of columns in board
+     * @param semester the number of the current semester.
+     * @param validStudentsNum the number of valid students on the board.
+     * @param changes the number of changes in the current semester
+     * The function checks if the game is still on by these rules:
+     * haven't reached the semester number limit.
+     * there were changes in the past semester.
+     * there's at least one valid students on board.
+     * @return the function returns true if the game can continue false otherwise
+     */
+    public static boolean isGameContinues(int[][] board , int m, int n, int semester, int validStudentsNum, int changes) {
         if (changes == 0) {
             System.out.println("The students have stabilized.");
             return false;
         }
-        printStatus(board,m,n,validStudentsNum,semesterNum);
-        if (semesterNum == SEMESTER_MAX) {
+        printStatus(board,m,n,validStudentsNum,semester);
+        if (semester == SEMESTER_MAX) {
             System.out.println("The semesters limitation is over.");
             return false;
         }
@@ -155,7 +207,9 @@ public class Main {
         return true;
     }
 
-
+    /**
+     * The main function of the game.
+     */
     public static void theStudentsGame() {
         int m, n, validStudentsNum, semester = 1, changes = 1;
         int[] mn = new int[2];
@@ -175,13 +229,6 @@ public class Main {
             changes = semesterChanges(board, validFriends, m, n);
             semester++;
             validStudentsNum = countValidFriends(board, validFriends, m, n);
-
-            /**do  {
-             printStatus(board, m,n,validStudentsNum,semester);
-             changes = semesterChanges(board, validFriends, m, n);
-             semester++;
-             validStudentsNum = countValidFriends(board, validFriends, m, n);
-             } while(isGameContinues(semester, validStudentsNum, changes));*/
         }
     }
 
